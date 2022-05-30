2e7a67e1a10cab44699c629075e99c44fdc30b67
L-L-0-Y-D <agorita28@gmail.com>
2022-05-30 21:54:02 +0800

    Index admin

--- admin/busiowner.php
+++ admin/busiowner.php
@@ -350,8 +350,9 @@
                     <div class="text-center my-auto copyright"><span>Copyright © I - Eat 2022</span></div>
                 </div>
             </footer>
-        </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
+        <div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
     </div>
+</div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="assets/js/bs-init.js"></script>
     <script src="assets/js/theme.js"></script>
--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -1,10 +1,11 @@
-<div class="container-fluid d-flex flex-column p-0"><a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-1" href="index.php">
-                <div class="sidebar-brand-icon"><img class=" img-profile" src="../uploads/I-EatLogo.png" width="80" height="80"></div>
-                <div class="sidebar-brand-text mx-1"><span>I - Eat</span></div>
+<nav class="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0" style="background: rgb(255,128,64);">
+            <div class="container-fluid d-flex flex-column p-0"><a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-1" href="#">
+                    <div class="sidebar-brand-icon"><img class=" img-profile" src="../uploads/I-EatLogo.png" width="80" height="80"></div>
+                    <div class="sidebar-brand-text mx-1"><span>I - Eat</span></div>
                 </a>
                 <hr class="sidebar-divider my-0">
                 <ul class="navbar-nav text-light" id="accordionSidebar">
-                    <li class="nav-item"><a class="nav-link" href="../home.php"><i class="fas fa-home"></i><span>Home</span></a></li>
+                    <li class="nav-item"><a class="nav-link " href="../home.php"><i class="fas fa-home"></i><span>Home</span></a></li>
                     <li class="nav-item"><a class="nav-link active" href="index.php"><i class="fas fa-tachometer-alt"></i><span>Dashboard</span></a></li>
                     <li class="nav-item"><a class="nav-link" href="busiowner.php"><i class="fas fa-user-friends"></i><span>Business Owners</span></a></li>
                     <li class="nav-item"><a class="nav-link" href="customers.php"><i class="fas fa-table"></i><span>Customers</span></a></li>
