9c3f3a36b47eec67938d81d927b95cb91cc4da6b
L-L-0-Y-D <agorita28@gmail.com>
2022-05-30 19:59:23 +0800

    middleware

---
+++ middleware/adminMiddleware.php
@@ -0,0 +1,21 @@
+<?php
+include('../functions/myfunctions.php');
+
+/* This is checking if the user is logged in and if the user is an admin. If the user is not logged in,
+they are redirected to the login page. If the user is logged in but not an admin, they are
+redirected to the index page. */
+if(isset($_SESSION['auth'])){
+
+    if($_SESSION['role_as'] != 1){
+
+    redirect("../index.php", "You are not authorized to access this page");
+
+    }
+
+}else{
+
+    redirect("../login.php", "Login to Continue");
+
+}
+
+?>
