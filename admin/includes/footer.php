5b59dcfd4e10666073545a87fd5a509cde83676c
L-L-0-Y-D <agorita28@gmail.com>
2022-05-30 20:28:41 +0800

    adding js files

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -9,6 +9,35 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js"></script>
     <script src="assets/js/bs-init.js"></script>
     <script src="assets/js/theme.js"></script>
-</body>
 
+    
+    <!--   Core JS Files   -->
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/perfect-scrollbar.min.js"></script>
+    <script src="assets/js/smooth-scrollbar.min.js"></script>
+
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="assets/js/custom.js"></script>
+
+
+
+    <!-- ALERTIFY JavaScript -->
+    <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
+
+    <!--Balikan na lang Hindi Gumagana PART8 sa video-->
+    <script>
+    <?php if(isset($_SESSION['message'])) 
+    { ?>
+        alertify.set('notifier','position', 'top-right');
+        alertify.success('<?= $_SESSION['message']; ?>');
+        <?php 
+        unset($_SESSION['message']);
+    }
+    
+    ?>
+    </script>
+
+    
+</body>
 </html>
