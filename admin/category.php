69949c4faadef313ed77cbc83a4feabbe2b3e56b
L-L-0-Y-D <agorita28@gmail.com>
2022-06-01 18:46:13 +0800

    fix bug

--- admin/category.php
+++ admin/category.php
@@ -40,9 +40,7 @@ include('includes/header.php');
                                 
                                 <tbody style="text-align:center">
                                     <?php
-                                        $cuisine = getAll("mealcategory");
-
-<<<<<<< HEAD
+                                        $cuisine = getAll("mealcategory"); 
                                         if(mysqli_num_rows($cuisine ) > 0)
                                         {
                                             foreach($cuisine  as $item)
@@ -74,40 +72,8 @@ include('includes/header.php');
                             </table>
                         </div>
                         <div class="row">
-                            <div class="col-md-6 align-self-center">
-                                <p id="dataTable_info" class="dataTables_info" role="status" aria-live="polite">Showing 1 to 10 of 27</p>
-=======
-                               if(mysqli_num_rows($cuisine ) > 0)
-                               {
-                                   foreach($cuisine  as $item)
-                                   {
-                                       if($_SESSION['role_as'] != 0)
-                                       {
-                                       ?>
-                                       <tr>
-                                           <td><?= $item['categoryid']; ?></td>
-                                           <td><?= $item['categoryname']; ?></td>
-                                           <td><?= $item['status']== '0'? "Active":"Hidden"  ?></td>
-                                           <td>
-                                               <a href="edit-category.php?id=<?= $item['categoryid']; ?>" class="btn btn-sm btn-primary">Edit</a>
-                                           </td>
-                                           <td>
-                                               <button type="button" class="btn btn-sm btn-danger delete_category_btn" value="<?= $item['categoryid']; ?>" >Delete</button>
-                                           </td>
-                                       </tr>
-                                       <?php
-                                       }
-                                   }
-                               }
-                                else
-                                {
-                                    echo "No records Found";
-                                }
-                           ?>
-                                    </tbody>
-                                </table>
->>>>>>> dd9a63bb1273e50682fde8378c01d8e83cb4de4c
-                            </div>
+            
+                            <div>
                             <div class="col-md-6">
                                 <nav class="d-lg-flex justify-content-lg-end dataTables_paginate paging_simple_numbers">
                                     <ul class="pagination">
