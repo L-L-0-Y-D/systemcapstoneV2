b3475e8c04793341f870852268ee305efdc63fce
L-L-0-Y-D <agorita28@gmail.com>
2022-06-04 20:20:53 +0800

    business

--- business.php
+++ business.php
@@ -7,16 +7,17 @@
 ?>
     <div class="container"><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
         <?php 
-            if(isset($_GET['id']))
+        if(isset($_GET['id']))
             {
                 $id = $_GET['id'];
                 $municipality = getByIDActive("municipality",$id,"municipalityid");
 
-                if(mysqli_num_rows($municipality) > 0)
-                {
-                    $data = mysqli_fetch_array($municipality);
-                    $mid = $data['municipalityid'];
-            ?>
+                    if(mysqli_num_rows($municipality) > 0)
+                        {
+                            $data = mysqli_fetch_array($municipality);
+                            $mid = $data['municipalityid'];
+        
+        ?>
         <h1 style="text-align: center;width: 753.453px;margin-left: 200px;font-size: 36px;"><strong>Restaurants in <?= $data['municipality_name']; ?></strong></h1>
         <div class="collapse navbar-collapse" id="navcol-1">
             <ul class="navbar-nav ms-auto"></ul>
@@ -29,47 +30,44 @@
                     <div class="row">
                         <div class="col-md-9">
                             <div class="products" style="margin-left: 190px;">
-                            <?php
-                                    $business = getBusiByMunicipality($mid);
-
-                                        if(mysqli_num_rows($business) > 0)
-                                        {
-                                            foreach($business as $item)
-                                            {
-                                    ?>
                                 <div class="row g-0" style="width: 729px;">
                                     <div class="col-12 col-md-6 col-lg-4">
-                                    <a href="#">
                                         <div class="clean-product-item" style="padding: 10px;">
+                                        <?php
+                                        $business = getBusiByMunicipality($mid);
+
+                                            if(mysqli_num_rows($business) > 0)
+                                            {
+                                                foreach($business as $item)
+                                                {
+                                                    ?>
                                             <div class="image"><a href="#"><img class="img-fluid d-block mx-auto" src="uploads/<?= $item['image']; ?>"></a></div>
-                                            <div class="product-name" style="margin-bottom: 0px;"><a href="#"><strong><?= $item['business_name']; ?></strong></a></div><small>Located at <?= $item['business_name']; ?> <?= $data['municipality_name']; ?>, Bataan</small>
+                                            <div class="product-name" style="margin-bottom: 0px;"><a href="#"><strong><?= $item['business_name']; ?></strong></a></div><small>Located at <?= $item['business_address']; ?> <?= $data['municipality_name']; ?>, Bataan</small>
                                             <div class="about">
                                                 <div class="rating"><img src="assets/img/star.svg"><img src="assets/img/star.svg"><img src="assets/img/star.svg"><img src="assets/img/star-half-empty.svg"><img src="assets/img/star-empty.svg"></div>
                                                 <div class="price"></div>
                                             </div><button class="btn btn-primary" type="button" style="font-size: 13px;background: var(--bs-orange);border-color: var(--bs-orange);border-radius: 12px;margin-left: 77px;margin-top: 4px;">Make Reservation</button>
-                                        <a>
+                                            <?php
+                                                            }
+                                                        }
+                                                        else
+                                                        {
+                                                            echo "No Business Found";
+                                                        }
+                                                    }
+                                                    else
+                                                    {
+                                                        echo "Places not Found";
+                                                    }
+                                                }
+                                                else
+                                                {
+                                                    echo"Something went wrong";
+                                                }
+                                            ?>
                                         </div>
                                     </div>
-                                    <?php
-                                                }
-                                            }
-                                            else
-                                            {
-                                                echo "No Business Found";
-                                            }
-                                        }
-                                        else
-                                        {
-                                            echo "Places not Found";
-                                        }
-                                    }
-                                    else
-                                    {
-                                        echo"Something went wrong";
-                                    }
-                                ?>
-                                </div>
-                                <!--<nav>
+                                <nav>
                                     <ul class="pagination">
                                         <li class="page-item disabled"><a class="page-link" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
                                         <li class="page-item active"><a class="page-link">1</a></li>
@@ -77,7 +75,7 @@
                                         <li class="page-item"><a class="page-link">3</a></li>
                                         <li class="page-item"><a class="page-link" aria-label="Next"><span aria-hidden="true">»</span></a></li>
                                     </ul>
-                                </nav>-->
+                                </nav>
                             </div>
                         </div>
                     </div>
@@ -85,5 +83,4 @@
             </div>
         </section>
     </main>
-
 <?php include('includes/footer.php');?>
