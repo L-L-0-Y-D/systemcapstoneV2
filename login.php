5504dfbee496de50b5d9378e85259c51c8a99ad0
Melodee21 <mnbantog@bpsu.edu.ph>
2022-06-16 18:40:07 +0800

    login

--- login.php
+++ login.php
@@ -16,38 +16,51 @@ if(isset($_SESSION['auth'])){
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="login.css"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 shrink-to-fit=no">
     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/default.min.css"/>
     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/bootstrap.min.css"/>
-    <title>Login Customer Account</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/Montserrat.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.11.1/baguetteBox.min.css">
+    <link rel="stylesheet" href="assets/css/vanilla-zoom.min.css">
+    
+    <title>Login | I-Eat</title>
 
 </head>
-<body>
-    <header>
-         <img src="uploads/I-EatLogo.png" alt="LOGO" usemap="#workmap" width="200" height="200">
-    <map name="workmap">
-        <area shape="circle" coords="100,100,400,400" alt="logo" href="index.php">
-    </map>
-    </header>
-<main>
-    <form method="post" action="functions/authcode.php" > 
+<body style="background: url(uploads/bg.png) center / cover no-repeat;">
+    <main class="page registration-page">
+        <section class="clean-block clean-form dark" style="height: 672px;background: transparent;">
+                <img class="img-fluid d-flex d-lg-flex align-items-center m-auto" src="uploads/I-EatLogo.png" width="200px" height="200px" alt="logo" usemap="#workmap">
+                    <map name="workmap">
+                        <area shape="circle" coords="100,100,400,400" alt="logo" href="index.php">
+                    </map>
+        <form method="post" action="functions/authcode.php" style="background: rgb(255, 128, 64);border-style: solid;border-color: rgb(255, 128, 64);border-top-color: rgb(255,;border-right-color: 128,;border-bottom-color: 64);border-left-color: 128,;border-radius: 20px;"> 
         <div class="container">
-            <p>LOGIN</p>
+            <h2 class="d-flex justify-content-center" style="font-weight:bold;">LOGIN</h2>
             <div class="row">
-                <input type="email" name="email" required placeholder="Email" class="input"/>
+                <div class="mb-3">
+                        <label class="form-label" for="email" style="font-weight: bold;">Email</label>
+                        <input type="email" name="email" id="email" class="form-control form-control-sm item" style="font-size: 14px;height: 50px;" required/>
+                </div>
             </div>
             <div class="row">
-                <input type="password" name="password" placeholder="Password" class="input"/>
+                <div class="mb-3">
+                    <label class="form-label" for="password" style="font-weight: bold;">Password</label>
+                    <input type="password" name="password" id="password" class="form-control form-control-sm item" style="height: 50px;" required/>
+                </div>
             </div>
-                <button class="login-btn" name='login_btn' >LOGIN</button> <br> <br>
+                <button class="btn btn-primary d-flex d-xl-flex align-items-center m-auto" name='login_btn'style="background:black;color:white;border-style: none;border-bottom-style: none;padding-right: 15px;padding-left: 15px;font-size: 18px;padding-bottom: 7px;">LOGIN</button> <br>
                 <!--link to connect with register php-->
-                <a href="register.php">Create an account</a>
+                <a class="d-flex justify-content-center text-black" href="register.php">Create an account</a>
         </div>
-    </form>
+        </form>
+    </section>
 </main>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.11.1/baguetteBox.min.js"></script>
+    <script src="assets/js/vanilla-zoom.js"></script>
+    <script src="assets/js/theme.js"></script>
     <!-- Optional JavaScript; choose one of the two! -->
     <script src="assets/js/jquery-3.6.0.min.js"></script>
     <script src="assets/js/bootstrap.bundle.min.js"></script>
