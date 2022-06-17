122304aaff779978d3b2690712bae0e80c588f86
Melodee21 <mnbantog@bpsu.edu.ph>
2022-06-18 00:25:32 +0800

    dgs

--- register.php
+++ register.php
@@ -33,7 +33,7 @@ if(isset($_SESSION['auth'])){
                 <map name="workmap">
                     <area shape="circle" coords="100,100,400,400" alt="logo" href="index.php">
                 </map>
-                <form method="post" action="functions/authcode.php" enctype="multipart/form-data" style="background: rgb(255, 128, 64);border-style: solid;border-color: rgb(255, 128, 64);border-top-color: rgb(255,;border-right-color: 128,;border-bottom-color: 64);border-left-color: 128,;border-radius: 20px;">
+                <form method="post" action="functions/authcode.php" enctype="multipart/form-data" style="background: rgb(255, 128, 64);border:none;border-radius: 20px;">
                     <div class="container">
                         <h2 class="d-flex justify-content-center" style="font-weight:bold;">REGISTER</h2>
                         <!-- Input Image -->
