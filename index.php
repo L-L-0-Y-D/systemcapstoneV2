06c8881a55b1c1ef3b856fba34358070a27baea6
Melodee21 <mnbantog@bpsu.edu.ph>
2022-06-17 23:59:44 +0800

    forms

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <div class="heading">
-                    <h2>Municipalities</h2>
+                <div class="heading ">
+                    <h2 style="font-weight:bold; font-family:Sans Serif">Municipalities</h2>
                 </div>
                 <hr>
                 <div class="row justify-content-center">
@@ -26,7 +26,7 @@
                             ?>
                                 <div class="col-md-12 col-lg-4 project-sidebar-card" >
                                     <a href="business.php?id=<?= $item['municipalityid']; ?>">
-                                    <div class="card" data-bss-hover-animate="pulse" style="height:300px; weight:300px;">
+                                    <div class="card" data-bss-hover-animate="pulse" style="height:300px; weight:300px; margin-bottom:10px;">
                                         <img class="img-fluid card-img w-100 h-100 d-block" src="uploads/<?= $item['image']; ?>" alt="Municipality Image" height="300px" width="300px" >
                                         <div class="card-img-overlay" >
                                             <h4 class="display-6  fw-bold" style="margin-top: 220px; color:white;"><?= $item['municipality_name']; ?></h4>
@@ -50,50 +50,5 @@
 </div>
 </div>
 </div>
-<!--<section class="clean-block about-us">
-            <div class="container">
-                <div class="block-heading">
-                    <h2 class="fw-bold" style="color:rgb(255,128,64);">About Us</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo.</p>
-                </div>
-                <div class="row justify-content-center">
-                    <div class="col-sm-3">
-                        <div class="card text-center clean-card"><img class="card-img-top w-100 d-block" src="assets/img/avatars/avatar1.jpg">
-                            <div class="card-body info">
-                                <h4 class="card-title">John Lloyd Agorita</h4>
-                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                                <div class="icons"><a href="#"><i class="icon-social-facebook"></i></a><a href="#"><i class="icon-social-instagram"></i></a><a href="#"><i class="icon-social-twitter"></i></a></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-3">
-                        <div class="card text-center clean-card"><img class="card-img-top w-100 d-block" src="assets/img/avatars/avatar2.jpg">
-                            <div class="card-body info">
-                                <h4 class="card-title">Robert Downturn</h4>
-                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                                <div class="icons"><a href="#"><i class="icon-social-facebook"></i></a><a href="#"><i class="icon-social-instagram"></i></a><a href="#"><i class="icon-social-twitter"></i></a></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-3">
-                        <div class="card text-center clean-card"><img class="card-img-top w-100 d-block" src="assets/img/avatars/avatar3.jpg">
-                            <div class="card-body info">
-                                <h4 class="card-title">Ally Sanders</h4>
-                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                                <div class="icons"><a href="#"><i class="icon-social-facebook"></i></a><a href="#"><i class="icon-social-instagram"></i></a><a href="#"><i class="icon-social-twitter"></i></a></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-3">
-                        <div class="card text-center clean-card"><img class="card-img-top w-100 d-block" src="assets/img/avatars/avatar3.jpg">
-                            <div class="card-body info">
-                                <h4 class="card-title">Ally Sanders</h4>
-                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                                <div class="icons"><a href="#"><i class="icon-social-facebook"></i></a><a href="#"><i class="icon-social-instagram"></i></a><a href="#"><i class="icon-social-twitter"></i></a></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>-->
-        
+
 <?php include('includes/footer.php');?>
