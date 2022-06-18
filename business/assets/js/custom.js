5750e35bb9d0ee427fc07582034a3fdd533d9245
L-L-0-Y-D <agorita28@gmail.com>
2022-06-18 22:53:52 +0800

    remove

--- business/assets/js/custom.js
+++ business/assets/js/custom.js
@@ -1,51 +1,8 @@
 $(document).ready(function () {
 
     /* This is a jQuery function that is used to delete a product. */
-    $(document).on('click','.delete_product_btn', function (e){
 
-        e.preventDefault();
-
-        var id = $(this).val();
-        alert(id);
-
-        swal({
-            title: "Are you sure?",
-            text: "Once deleted, you will not be able to recover",
-            icon: "warning",
-            buttons: true,
-            dangerMode: true,
-          })
-          .then((willDelete) => {
-            if (willDelete) {
-                $.ajax({
-                    method: "POST",
-                    url: "code.php",
-                    data: {
-                        'productid':id,
-                        'delete_product_btn':true
-                    },
-                    success: function (response) {
-                        if(response == 200)
-                        {
-                            swal("");
-                            swal("Success!", "Product Deleted Successfully", "success");
-                            $("#products_table").load(location.href + " #products_table");
-                        }
-                        else if(response == 500)
-                        {
-                            swal("Error!", "Something went wrong", "error");
-                        }
-                }
-              });
-            }
-          });
-
-    });
-
-    
-    /* This is a jQuery function that is used to delete a category. */
-    $(document).on('click','.delete_category_btn', function (e){
-        
+    $('.delete_product_btn').click(function (e){
         e.preventDefault();
 
         var id = $(this).val();
@@ -53,7 +10,7 @@ $(document).ready(function () {
 
         swal({
             title: "Are you sure?",
-            text: "Once deleted, you will not be able to recover",
+            text: "Once deleted, you will not be able to recover the data",
             icon: "warning",
             buttons: true,
             dangerMode: true,
@@ -62,26 +19,73 @@ $(document).ready(function () {
             if (willDelete) {
                 $.ajax({
                     method: "POST",
-                    url: "code.php",
-                    data: {
-                        'category_id':id,
-                        'delete_category_btn':true
-                    },
-                    success: function (response) {
-                        if(response == 200)
-                        {
-                            swal("");
-                            swal("Success!", "Category Deleted Successfully", "success");
-                            $("#category_table").load(location.href + " #category_table");
+                        url: "code.php",
+                        data: {
+                            'productid':id,
+                            'delete_product_btn': true
+                        },
+                        success: function (response) {
+                            console.log(response);
+                            if(response == 200)
+                            {
+                                swal("Success!", "Product Deleted Successfully", "success");
+                                $("#products_table").load(location.href + " #products_table");
+                            }
+                            else if(response == 500)
+                            {
+                                swal("Success!", "Something went wrong", "error");
+                            }
+
                         }
-                        else if(response == 500)
-                        {
-                            swal("Error!", "Something went wrong", "error");
-                        }
-                }
-              });
+                    });
             }
           });
-
+        
     });
+
+    /* This is a jQuery function that is used to delete a product. */
+    $('.delete_reservation_btn').click(function (e){
+        e.preventDefault();
+
+        var id = $(this).val();
+        //alert(id);
+
+        swal({
+            title: "Are you sure?",
+            text: "Once deleted, you will not be able to recover the data",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+          })
+          .then((willDelete) => {
+            if (willDelete) {
+                $.ajax({
+                    method: "POST",
+                        url: "code.php",
+                        data: {
+                            'reservationid':id,
+                            'delete_reservation_btn': true
+                        },
+                        success: function (response) {
+                            console.log(response);
+                            if(response == 900)
+                            {
+                                swal("Success!", "Category Deleted Successfully", "success");
+                                $("#reservation_table").load(location.href + " #reservation_table");
+
+                            }
+                            else if(response == 800)
+                            {
+                                swal("Success!", "Something went wrong", "error");
+                            }
+
+                        }
+                    });
+            }
+          });
+        
+    });
+
+    
+    
 });
