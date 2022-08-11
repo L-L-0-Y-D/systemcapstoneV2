ce311cf28fa26695038c8b49552c3c4fafda9452
Melodee21 <mnbantog@bpsu.edu.ph>
2022-08-12 02:20:18 +0800

    business.php

--- business.php
+++ business.php
@@ -14,60 +14,51 @@
                         $business = getBusiByMunicipality($mid);
     
 ?>
-    <div class="container" >   
-        <h1 style="text-align: center;width: auto;font-size: 30px; padding-top:20px;"><strong>Restaurants in <?= $data['municipality_name']; ?></strong></h1>   
-        <hr>
-    </div>
-    <main class="page catalog-page">
-        <section class="portfolio-block project-with-sidebar">
-            <div class="container ">
-                <div class=" content">
-                    <div class="row" >
-                        <div class="col-md-9 d-flex " style="width: 100%;">
-                            <div class="products">
-                                <div class="row g-0 " style="padding-bottom: 50px;margin-left: 0px; border:none;">
-                                    <?php
-                                        if(mysqli_num_rows($business) > 0)
-                                            {
-                                                foreach($business as $item)
-                                                {
-                                                    ?>
-                                    <div class="col-12 col-md-6 mb-2 ml-2 col-lg-4" style="border:1px solid black; width: 250px;">
-                                        <div class="clean-product-item">
-                                        <a href="businessview.php?id=<?=$item['businessid'];?>">
-                                            <div class="image"><img class="img-fluid d-block mx-auto " src="uploads/<?= $item['image']; ?>" style="height:140px; width: 200px;"></div>
-                                            <div class="product-name mb-0">
-                                                <a class="d-flex" href="#"><strong><?= $item['business_name']; ?></strong></a></div>
-                                                <div class="product-details m-0"><small style="font-size:12px;">Located at <?= $item['business_address']; ?></small></div>
-                                                <div class="product-details m-0"><small style="font-size:12px;"><?= $item['cuisinename']; ?></small></div>
-                                                <div class="product-details m-0"><small style="font-size:12px;">Opening: <?= date("g:i a", strtotime($item['opening'])); ?>- Closing: <?= date("g:i a", strtotime($item['closing'])); ?></small></div>
-                                            <div class="about">
-                                                <!--<div class="rating"><img src="assets/img/star.svg"><img src="assets/img/star.svg"><img src="assets/img/star.svg"><img src="assets/img/star-half-empty.svg"><img src="assets/img/star-empty.svg"></div>-->
-                                               
-                                            </div>
-                                        </a>
-                                            <button href="" onclick="location='reservation.php?id=<?= $item['businessid']; ?>'" class="btn btn-primary text-center float-end" type="button" style="height: 29px;padding-top: 3px;background: RGB(255,128,64);border: 1px solid var(--bs-orange);border-radius: 20px;margin-left: 0px;font-size: 14px;width: 152.328px;text-align: left;margin-bottom: 13px;">Make Reservation</button>
-                                        </div>
-                                    </div>
-                                    <?php
-                                                }
-                                            }
-                                        else
-                                        {
-                                            echo "No Business Found";
-                                            ?>
-                                            <br><a href="index.php">Go Back</a>
-                                            <?php
-                                        }
-                                        ?>
-                                </div>
+<body>
+    <main class="page projects-page">
+        <section class="portfolio-block projects-cards" style="padding-top: 40px;">
+            <div class="container">
+                <div class="heading">
+                    <h2 style="margin-bottom: 20px;font-size: 60px;font-weight: bold;font-family: Acme, sans-serif;text-align: center;">Restaurants in <?= $data['municipality_name']; ?></h2>
+                </div>
+                <div class="row">
+                    <?php
+                        if(mysqli_num_rows($business) > 0)
+                        {
+                        foreach($business as $item)
+                        {
+                    ?>
+                    <div class="col-md-6 col-lg-4">
+                        <div class="card border-5"><a href="businessview.php?id=<?=$item['businessid'];?>">
+                            <img class="card-img-top scale-on-hover" src="uploads/<?= $item['image']; ?>" alt="Card Image"></a>
+                            <div class="card-body" style="padding-top: 10px;">
+                                <p class="text-center" style="font-family: Acme, sans-serif;font-weight: bold;font-size: 20px;"><?= $item['business_name']; ?></p>
+                                <p class="text-muted card-text" style="margin-bottom: 0px;text-align: left;">Located at <?= $item['business_address']; ?></p>
+                                <p class="text-muted card-text" style="margin-bottom: 0px;text-align: left;">Opening: <?= date("g:i a", strtotime($item['opening'])); ?>- Closing: <?= date("g:i a", strtotime($item['closing'])); ?></p>
+                                <p class="text-muted card-text" style="text-align: left;"><?= $item['cuisinename']; ?> Cuisine</p>
+                                <button onclick="location='reservation.php?id=<?= $item['businessid']; ?>'" class="btn btn-primary text-center" type="button" style="height: 29px;padding-top: 3px;background: RGB(255,128,64);border: 1px solid var(--bs-orange);border-radius: 20px;margin-left: 0px;font-size: 14px;width: 152.328px;margin-bottom: 13px;">Make Reservation</button>
                             </div>
                         </div>
                     </div>
+                    <?php
+                    }
+                        }
+                        else
+                        {
+                            echo "No Business Found";
+                        ?>
+                        <br><a href="index.php">Go Back</a>
+                        <?php
+                        }
+                    ?>
                 </div>
             </div>
         </section>
     </main>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.6.1/pikaday.min.js"></script>
+    <script src="assets/js/theme.js"></script>
+</body>
     <?php
         } 
         else
--- includes/header.php
+++ includes/header.php
@@ -38,7 +38,7 @@
     <!-- Bootstrap CSS -->
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/custom.css" rel="stylesheet">
-
+    <link rel="stylesheet" href="assets/css/Acme.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
