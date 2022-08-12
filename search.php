892884957ba656ba45707b3afc75d733179cb0b0
L-L-0-Y-D <agorita28@gmail.com>
2022-08-12 15:06:21 +0800

    update on search

--- search.php
+++ search.php
@@ -19,15 +19,15 @@
                                 <div class="row g-0" style="padding: 0px;margin-left: 10px; border:none;">
                                     <?php
                                         /* A PDO connection to the database. */
-                                        $con = new PDO("mysql:host=localhost;dbname=thesis",'root','');
-                                        $con->setAttribute(PDO:: ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                                       $conn = new PDO("mysql:host=localhost;dbname=thesis",'root','');
+                                       $conn->setAttribute(PDO:: ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                                     
                                         /* This is the code that is responsible for the search for business
                                         function. */
                                         if (isset($_POST["submit"]))
                                         {
                                             $key = $_POST["search"];
-                                            $query = $con->prepare("SELECT * FROM business WHERE business_name LIKE :keyword AND status= '1' ORDER BY business_name ");
+                                            $query =$conn->prepare("SELECT * FROM business WHERE business_name LIKE :keyword AND status= '1' ORDER BY business_name ");
                                             $query->bindValue(':keyword' ,'%'.$key.'%', PDO::PARAM_STR);
                                             $query->execute();
                                             $results = $query->fetchAll();
@@ -72,7 +72,7 @@
                                         if (isset($_POST["submit"]))
                                         {
                                             $key = $_POST["search"];
-                                            $query = $con->prepare("SELECT * FROM business WHERE cuisinename LIKE :keyword AND status= '1' ORDER BY cuisinename ");
+                                            $query =$conn->prepare("SELECT * FROM business WHERE cuisinename LIKE :keyword AND status= '1' ORDER BY cuisinename ");
                                             $query->bindValue(':keyword' ,'%'.$key.'%', PDO::PARAM_STR);
                                             $query->execute();
                                             $results = $query->fetchAll();
@@ -117,7 +117,7 @@
                                         if (isset($_POST["submit"]))
                                         {
                                             $key = $_POST["search"];
-                                            $query = $con->prepare("SELECT business.businessid,business.business_name,business.business_address,business.opening,business.closing,business.municipalityid,municipality.municipality_name,business.cuisinename,business.image_cert,business.business_firstname,business.business_lastname,business.business_phonenumber,business.business_owneraddress,business.business_email,business.business_password,business.image,business.role_as,business.status,business.created_at FROM business JOIN municipality ON business.municipalityid=municipality.municipalityid AND business.status = '1' WHERE municipality_name LIKE :keyword ORDER BY municipality_name");
+                                            $query =$conn->prepare("SELECT business.businessid,business.business_name,business.business_address,business.opening,business.closing,business.municipalityid,municipality.municipality_name,business.cuisinename,business.image_cert,business.business_firstname,business.business_lastname,business.business_phonenumber,business.business_owneraddress,business.business_email,business.business_password,business.image,business.role_as,business.status,business.created_at FROM business JOIN municipality ON business.municipalityid=municipality.municipalityid AND business.status = '1' WHERE municipality_name LIKE :keyword ORDER BY municipality_name");
                                             $query->bindValue(':keyword' ,'%'.$key.'%', PDO::PARAM_STR);
                                             $query->execute();
                                             $results = $query->fetchAll();
@@ -154,6 +154,52 @@
                                             ?>
                                             
                                             <?php
+                                        }
+                                        ?>
+                                        <?php
+                                        /* This is the code that is responsible for the search fo municipality
+                                            function. */
+                                            if (isset($_POST["submit"]))
+                                            {
+                                                $key = $_POST["search"];
+                                                $query =$conn->prepare("SELECT products.productid,products.businessid,business.business_name,business.business_address,business.opening,business.closing,business.cuisinename,business.image_cert,business.business_firstname,business.business_lastname,business.business_phonenumber,business.business_owneraddress,business.business_email,business.business_password,business.image,business.role_as,business.status,business.created_at,products.name FROM products JOIN business ON products.businessid=business.businessid AND business.status = '1' WHERE name LIKE :keyword ORDER BY name");
+                                                $query->bindValue(':keyword' ,'%'.$key.'%', PDO::PARAM_STR);
+                                                $query->execute();
+                                                $results = $query->fetchAll();
+                                                $row = $query->rowCount();
+    
+                                            if($row!= 0)
+                                                {
+                                                    foreach($results as $item)
+                                                    {
+                                                        ?>
+                                       <div class="col-12 col-md-6 mb-2 ml-2 col-lg-4" style="border:1px solid black; width: 250px;">
+                                            <div class="clean-product-item">
+                                            <a href="businessview.php?id=<?=$item['businessid'];?>">
+                                                <div class="image"><img class="img-fluid d-block mx-auto " src="uploads/<?= $item['image']; ?>" style="height:140px; width: 200px;"></div>
+                                                <div class="product-name mb-0">
+                                                    <a class="d-flex" href="#"><strong><?= $item['business_name']; ?></strong></a></div>
+                                                    <div class="product-details m-0"><small style="font-size:12px;">Located at <?= $item['business_address']; ?></small></div>
+                                                    <div class="product-details m-0"><small style="font-size:12px;"><?= $item['cuisinename']; ?></small></div>
+                                                    <div class="product-details m-0"><small style="font-size:12px;">Opening: <?= date("g:i a", strtotime($item['opening'])); ?>- Closing: <?= date("g:i a", strtotime($item['closing'])); ?></small></div>
+                                                <div class="about">
+                                                    <!--<div class="rating"><img src="assets/img/star.svg"><img src="assets/img/star.svg"><img src="assets/img/star.svg"><img src="assets/img/star-half-empty.svg"><img src="assets/img/star-empty.svg"></div>-->
+                                                   
+                                                </div>
+                                            </a>
+                                                <button href="" onclick="location='reservation.php?id=<?= $item['businessid']; ?>'" class="btn btn-primary text-center float-end" type="button" style="height: 29px;padding-top: 3px;background: RGB(255,128,64);border: 1px solid var(--bs-orange);border-radius: 20px;margin-left: 0px;font-size: 14px;width: 152.328px;text-align: left;margin-bottom: 13px;">Make Reservation</button>
+                                            </div>
+                                        </div>
+                                        <?php
+                                                    }
+                                                }
+                                            else
+                                            {
+                                                
+                                                ?>
+                                                
+                                                <?php
+                                            }
                                         }
                                     }
                                 }
