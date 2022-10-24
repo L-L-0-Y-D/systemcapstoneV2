abfc5a0601ccd6c034f816f939a38275acea20a6
L-L-0-Y-D <agorita28@gmail.com>
2022-10-25 00:14:18 +0800

    test

--- config/dbcon.php
+++ config/dbcon.php
@@ -1,7 +1,7 @@
 <?php
 
 /* This is the code that connects to the database. */
-    $host = "localhost";
+    $host = "ieat.store";
     $username = "u217632220_ieat";
     $password = "Hj1@8QuF3C";
     $database = "u217632220_ieatwebsite";
