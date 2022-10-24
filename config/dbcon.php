30891204093323163d45202f86bc4c4b7151d0b1
L-L-0-Y-D <agorita28@gmail.com>
2022-10-24 21:45:24 +0800

    database

--- config/dbcon.php
+++ config/dbcon.php
@@ -1,10 +1,10 @@
 <?php
 
 /* This is the code that connects to the database. */
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "thesis";
+    $host = "ieat.store";
+    $username = "u217632220_ieat";
+    $password = "Hj1@8QuF3C";
+    $database = "u217632220_ieat";
 
     // Creating database connection
     $con = mysqli_connect($host,$username,$password,$database);
