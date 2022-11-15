85f8631fedd263b9c19078a3472b87c0f2836d70
L-L-0-Y-D <agorita28@gmail.com>
2022-11-15 22:21:17 +0800

    blockdates

---
+++ business/blockdate.php
@@ -0,0 +1,121 @@
+<?php 
+include('../middleware/businessMiddleware.php');
+include('includes/header.php');
+include('../config/dbcon.php');
+
+
+?>
+ <?php 
+        if(isset($_GET['id']))
+        {
+            $businessuserid = $_SESSION['auth_user']['businessid'];
+            $query_blockdates = "SELECT * FROM blockdate WHERE businessid = $businessuserid";
+            $query_blockdates_run = mysqli_query($con, $query_blockdates);
+            $blockdates = $query_blockdates_run;
+            $data = mysqli_fetch_array($blockdates);    
+?>
+    <div class="container-fluid">
+        <div class="d-sm-flex justify-content-between align-items-center mb-4">
+        <h3 class="text-dark mb-4">Block Date</h3>
+        <a class="btn btn-primary btn-sm d-none d-sm-inline-block" role="button" href="add-blockdate.php?id=<?= $_SESSION['auth_user']['businessid'];?>" style="background: rgb(255,128,64);border-style: none;"  id="addbtn">&nbsp;Add block date</a> 
+        </div>
+        <div class="card shadow">
+            <div class="card-body" id="reservation_table">
+                <div class="row">
+                    <div class="col-md-6 text-nowrap">
+                        <div id="dataTable_length" class="dataTables_length" aria-controls="dataTable"><label class="form-label">Show&nbsp;<select class="d-inline-block form-select form-select-sm">
+                            <option value="10" selected="">10</option>
+                            <option value="25">25</option>
+                            <option value="50">50</option>
+                            <option value="100">100</option>
+                            </select>&nbsp;</label>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <!-- <div class="text-md-end dataTables_filter" id="dataTable_filter"><label class="form-label"><input type="search" class="form-control form-control-sm" aria-controls="dataTable" placeholder="Search"></label></div> -->
+                        </div>
+                    </div>
+                    <div class="table-responsive table mt-2" id="dataTable" role="grid" aria-describedby="dataTable_info">
+                        <table class="table my-0" id="dataTable" style="text-align:center">
+                            <thead>
+                                <tr>
+                                    <th>Reason</th>
+                                    <th>Date</th>
+                                    <th>Status</th>
+                                    <th>View</th>
+                                    <!-- <th>Delete</th> -->
+                                </tr>
+                            </thead>
+                            
+                            <tbody style="text-align:center">
+                                <?php
+                                    // $reservations = getAll("reservations");
+                                    //$reservations = getAll("reservations");
+                                     
+                                    if(mysqli_num_rows($blockdates) > 0)
+                                    {
+                                        foreach($blockdates as $item)
+                                        {
+                                            if($item['businessid'] == $_SESSION['auth_user']['businessid'])
+                                                {
+                                                ?>
+                                                    
+                                                        <td><?= $item['reason']; ?></td>
+                                                        <td><?= $item['blockdates']; ?></td>
+                                                        <td><?php 
+                                                                if($item['status'] == 0)
+                                                                    { echo 'Waiting'; } 
+                                                                elseif($item['status'] == 1)
+                                                                    { echo 'Approved';}
+                                                                elseif($item['status'] == 2)
+                                                                    {echo 'Declined';}  
+                                                        ?></td>                                                                                                         
+                                                        <td>
+                                                            <a href="edit-reservation.php?id=<?= $item['blockdateid']; ?>" class="btn btn-sm btn-primary">View</a>
+                                                        </td>
+                                                        <!-- <td>
+                                                            <button type="button" class="btn btn-sm btn-danger delete_reservation_btn" value="<?=$item['reservationid'];?>">Delete</button>
+                                                        </td> -->
+                                                    </tr>
+                                                <?php
+                                                }
+                                        }
+                                    }
+                                    else
+                                    {
+                                        echo "No records Found";
+                                    }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 align-self-center">
+                            <p id="dataTable_info" class="dataTables_info" role="status" aria-live="polite">Showing 1 to 10 of 27</p>
+                        </div>
+                        <div class="col-md-6">
+                            <nav class="d-lg-flex justify-content-lg-end dataTables_paginate paging_simple_numbers">
+                                <ul class="pagination">
+                                    <li class="page-item disabled"><a class="page-link" aria-label="Previous" href="#"><span aria-hidden="true">«</span></a></li>
+                                    <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                    <li class="page-item"><a class="page-link" aria-label="Next" href="#"><span aria-hidden="true">»</span></a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php
+
+        }
+        else
+        {
+            echo"ID missing from url";
+        }
+?>
+<?php include('includes/footer.php');?>
