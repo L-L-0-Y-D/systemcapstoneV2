170e9f2fe8baf2e9baac56b125efc47a1a17c3db
L-L-0-Y-D <agorita28@gmail.com>
2022-11-21 20:58:33 +0800

    change

--- functions/userfunctions.php
+++ functions/userfunctions.php
@@ -1,5 +1,5 @@
 <?php
-
+$key = 'qkwjdiw239&&jdafweihbrhnan&^%$ggdnawhd4njshjwuuO';
 session_start();
 include('config/dbcon.php');
 
@@ -222,6 +222,26 @@ function redirect($url, $message, $alert)
     exit();
 }
 
+function encryptthis($data, $key) {
 
+    $encryption_key = base64_decode($key);
+    
+    $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc'));
+    
+    $encrypted = openssl_encrypt($data, 'aes-256-cbc', $encryption_key, 0, $iv);
+    
+    return base64_encode($encrypted . '::' . $iv);
+    
+    }
+
+function decryptthis($data, $key) {
+
+    $encryption_key = base64_decode($key);
+        
+    list($encrypted_data, $iv) = array_pad(explode('::', base64_decode($data), 2),2,null);
+        
+    return openssl_decrypt($encrypted_data, 'aes-256-cbc', $encryption_key, 0, $iv);
+        
+    }
 
 ?>
