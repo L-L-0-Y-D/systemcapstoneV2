92b02e9658ca375bf2c3cacca69cf16e71d77d40
Melodee21 <106127974+Melodee21@users.noreply.github.com>
2022-11-22 21:30:45 +0800

    decline

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -13,9 +13,9 @@
                     <li class="nav-item"><a class="nav-link <?= $page == "index.php"? 'active':'';  ?>" href="index.php"><i class="fas fa-bars"></i><span class="align-middle">&nbsp;Dashboard</span></a></li>
                     <li class="nav-item"><a class="nav-link <?= $page == "busiowner.php"? 'active':'';  ?>" href="busiowner.php"><i class="fas fa-handshake"></i><span class="align-middle">&nbsp;Business Owners</span></a></li>
                     <li class="nav-item"><a class="nav-link <?= $page == "customers.php"? 'active':'';  ?>" href="customers.php"><i class="fas fa-user-friends"></i><span class="align-middle">&nbsp;Customers</span></a></li>
-                    <li class="nav-item"><a class="nav-link <?= $page == "category.php"? 'active':'';  ?>" href="category.php"><i class="fas fa-bookmark"></i><span class="align-middle">&nbsp;Category</span></a></li>
+                    <li class="nav-item"><a class="nav-link <?= $page == "category.php"? 'active':'';  ?>" href="category.php"><i class="fas fa-store"></i><span class="align-middle">&nbsp;Category</span></a></li>
                     <li class="nav-item"><a class="nav-link <?= $page == "municipality.php"? 'active':'';  ?>" href="municipality.php"><i class="fas fa-map-marker"></i><span class="align-middle">&nbsp;Municipality</span></a></li>
-                    <li class="nav-item"><a class="nav-link <?= $page == "menu.php"? 'active':'';  ?>" href="menu.php"><i class="fas fa-store"></i><span class="align-middle">&nbsp;Menu</span></a></li>
+                    <li class="nav-item"><a class="nav-link <?= $page == "menu.php"? 'active':'';  ?>" href="menu.php"><i class="fas fa-utensils"></i><span class="align-middle">&nbsp;Menu</span></a></li>
                     <li class="nav-item"><a class="nav-link <?= $page == "reservation.php"? 'active':'';  ?>" href="reservation.php"><i class="fas fa-list"></i><span class="align-middle">&nbsp;Reservation</span></a></li>
                     <li class="nav-item"><a class="nav-link <?= $page == "feedback.php"? 'active':'';  ?>" href="feedback.php"><i class="far fa-file-alt"></i><span class="align-middle">&nbsp;Feedback</span></a></li>
                 </ul>
