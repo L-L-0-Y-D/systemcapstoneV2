89929202b85fc6b60039465d3df5d56f0b15134f
Melodee21 <106127974+Melodee21@users.noreply.github.com>
2022-11-23 21:36:29 +0800

    map

--- business/location.php
+++ business/location.php
@@ -23,9 +23,9 @@ if(isset($_GET['id']))
         <div class="row justify-content-center mb-5">
             <h4 class="text-dark">Business Location
             <a class="btn btn-primary float-end" role="button" href="pin-location.php?id=<?= $_SESSION['auth_user']['businessid'];?>" id="addbtn">Relocate</a> </h4>
-            <div class="col-md-12">
+
                 <iframe src="https://maps.google.com/maps?q=<?= $latitude ?>+<?= $longitude ?>&t=k&z=13&ie=UTF8&iwloc=&output=embed"></iframe>
-            </div>
+
         </div>
     </div>
 <?php
