7436f04a90e263eb86d2209bc7ff4f7b4e855f26
L-L-0-Y-D <agorita28@gmail.com>
2022-11-24 10:52:36 +0800

    as

--- your_reservation.php
+++ your_reservation.php
@@ -111,7 +111,7 @@ if(isset($_GET['id']))
                                         </div>
                                         <div class="col p-0"><hr class="mb-0">
                                             <p class="float-start m-2 text-muted">Reservation Id : #<?= $data['reservationid']; ?></p>
-                                            <p class="float-end m-2 text-muted">Status:&nbsp;<strong><?php if($data['status'] == 0){ echo 'Waiting'; } elseif($data['status'] == 1){ echo 'Approved';}elseif($data['status'] == 2){echo 'Declined';}elseif($data['status'] == 3){echo 'Cancelled';}  ?></strong></p>
+                                            <p class="float-end m-2 text-muted">Status:&nbsp;<strong><?php if($data['status'] == 0){ echo 'Waiting'; } elseif($data['status'] == 1){ echo 'Approved';}elseif($data['status'] == 2){echo 'Declined';}elseif($data['status'] == 3){echo 'Cancelled';}elseif($data['status'] == 1 && $data['arrived'] == 1){ echo 'For Review';}  ?></strong></p>
                                         </div><hr class="mt-0">
                                     </div>
                                 </div>
