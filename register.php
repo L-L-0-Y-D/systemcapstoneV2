3c3957e9176d957895ec4ef56b15c77e6513e262
karenguinto <120320475+karenguinto@users.noreply.github.com>
2022-12-12 11:12:04 +0800

    1

--- register.php
+++ register.php
@@ -45,7 +45,7 @@ if(isset($_SESSION['auth'])){
             background-position:center;
             background-repeat: no-repeat;
             background-size: cover;
-            padding:30px;
+           
             }
     </style>
 
