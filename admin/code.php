ad477f03cb14ce65562c651eda19316dd175d884
L-L-0-Y-D <agorita28@gmail.com>
2022-12-22 13:35:14 +0800

    asd

--- admin/code.php
+++ admin/code.php
@@ -944,7 +944,7 @@ else if(isset($_POST['edit_business_btn']))
     $reason = $_POST['reason'];
     $status = $_POST['status'];
 
-    if($status = 2 || $reason == '')
+    if($status = 2 && $reason == '')
     {
         redirect("edit-business.php?id=$businessid", "Please add reason for declined", "warning"); 
     } else {
