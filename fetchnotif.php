97a7ac5e3607b90f83792125c1c0d62697218088
L-L-0-Y-D <agorita28@gmail.com>
2022-12-25 18:18:29 +0800

    fetchnotif

---
+++ fetchnotif.php
@@ -0,0 +1,28 @@
+<?php
+//fetch.php;
+$host = "localhost";
+$username = "u217632220_ieat";
+$password = "Hj1@8QuF3C";
+$database = "u217632220_ieatwebsite";
+
+// Creating database connection
+$connect = mysqli_connect($host,$username,$password,$database);
+$query = "SELECT * FROM notifications WHERE comment_status = 0 ORDER BY comment_id DESC";
+$result = mysqli_query($connect, $query);
+$output = '';
+while($row = mysqli_fetch_array($result))
+{
+ $output .= '
+ <div class="alert alert_default">
+  <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+  <p><strong>'.$row["comment_subject"].'</strong><br>
+   <small><em>'.$row["comment_text"].'</em></small>
+  </p>
+ </div>
+ ';
+}
+$update_query = "UPDATE notifications SET comment_status = 1 WHERE comment_status = 0";
+mysqli_query($connect, $update_query);
+echo $output;
+
+?>
