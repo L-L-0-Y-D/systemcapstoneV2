b01b4dc72b1a7ce64bb386dd239e3c2e21589f65
L-L-0-Y-D <agorita28@gmail.com>
2023-01-05 23:12:55 +0800

    update change in password validation

--- assets/js/register-js.js
+++ assets/js/register-js.js
@@ -148,50 +148,52 @@ function checkPassword() {
 	if (passwordValue == ''){
 	setErrorFor(password, 'Please Enter A Password');
 	}else if (!PasswordValidation.test(passwordValue)){
+		// When the user starts to type something inside the password field
+	myInput.onkeyup = function()
+	{
+		// Validate Special Characters
+		var lowerCaseLetters = /[a-z]/g;
+		  if(myInput.value.match(lowerCaseLetters)) {
+		letter.classList.remove("invalid");
+		letter.classList.add("valid");
+		  } else {
+		letter.classList.remove("valid");
+		letter.classList.add("invalid");
+		}
+	
+		// Validate capital letters
+		var upperCaseLetters = /[A-Z]/g;
+		if(myInput.value.match(upperCaseLetters)) {  
+		capital.classList.remove("invalid");
+		capital.classList.add("valid");
+		} else {
+		capital.classList.remove("valid");
+		capital.classList.add("invalid");
+		}
+	
+		// Validate numbers
+		var numbers = /[0-9]/g;
+		if(myInput.value.match(numbers)) {  
+		number.classList.remove("invalid");
+		number.classList.add("valid");
+		} else {
+		number.classList.remove("valid");
+		number.classList.add("invalid");
+		}
+	
+		// Validate length
+		if(myInput.value.length >= 8) {
+		length.classList.remove("invalid");
+		length.classList.add("valid");
+		} else {
+		length.classList.remove("valid");
+		length.classList.add("invalid");
+		}
+	}
+
 		setErrorFor(password, '');  
 	}else{  
 		
-	// When the user starts to type something inside the password field
-	myInput.onkeyup = function() {
-	// Validate Special Characters
-	var lowerCaseLetters = /[a-z]/g;
-  	if(myInput.value.match(lowerCaseLetters)) {
-    letter.classList.remove("invalid");
-    letter.classList.add("valid");
-  	} else {
-    letter.classList.remove("valid");
-    letter.classList.add("invalid");
-	}
-
-	// Validate capital letters
-	var upperCaseLetters = /[A-Z]/g;
-	if(myInput.value.match(upperCaseLetters)) {  
-	capital.classList.remove("invalid");
-	capital.classList.add("valid");
-	} else {
-	capital.classList.remove("valid");
-	capital.classList.add("invalid");
-	}
-
-	// Validate numbers
-	var numbers = /[0-9]/g;
-	if(myInput.value.match(numbers)) {  
-	number.classList.remove("invalid");
-	number.classList.add("valid");
-	} else {
-	number.classList.remove("valid");
-	number.classList.add("invalid");
-	}
-
-	// Validate length
-	if(myInput.value.length >= 8) {
-	length.classList.remove("invalid");
-	length.classList.add("valid");
-	} else {
-	length.classList.remove("valid");
-	length.classList.add("invalid");
-	}
-	}
 	setSuccessFor(password); 
 		return true;
 	}	
