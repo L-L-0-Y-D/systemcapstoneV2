d742507a453ca040c9b5830d4c8c0e849f890cd1
Melodee21 <106127974+Melodee21@users.noreply.github.com>
2023-01-09 02:45:35 +0800

    1t

--- businessview.php
+++ businessview.php
@@ -56,7 +56,8 @@
     <link rel="stylesheet" href="assets/css/custom.css">
     <link rel="stylesheet" href="assets/css/Vujahday%20Script.css">
     <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
-    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>    
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script> 
+    <link rel="stylesheet" href="assets/css/menuview.css">   
     <title>I-Eat | Business View </title> 
     <!-- Favicon -->
     <link rel="icon" href="uploads/favicon.ico"/>
@@ -471,6 +472,9 @@
                     </div><hr class="m-0 w-100">
                 </div>
                  <!--END OF ABOUT SECTION-->
+                    <h1>Hello world</h1>
+                    <p class="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis veritatis animi aliquam laboriosam velit, esse, blanditiis aspernatur sint est magnam debitis delectus in fuga fugiat repellat dignissimos ipsum necessitatibus corrupti veniam reprehenderit,<span class="dots"> ...</span> <span class="moreText"> assumenda sapiente expedita labore atque! Sint velit cumque minus pariatur quisquam, beatae ab quo impedit eaque soluta vel laboriosam itaque similique iste ex aut in nihil dolorem consequuntur possimus eligendi eos optio ipsam! Sint ullam voluptate obcaecati asperiores eos vero sed iusto magnam ad, vel repellat quidem? Omnis fugit accusantium, illo quos eos odio consectetur et nemo excepturi deleniti dolorum adipisci dolores delectus possimus libero, sed iusto dolorem? Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis neque distinctio modi dicta ut impedit, vel dolore dolores maiores quis vitae nesciunt ab aut, praesentium facilis necessitatibus odio exercitationem velit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, error fugit repellat laborum aspernatur neque eligendi officia eius quidem voluptates fugiat numquam, officiis ullam nemo, incidunt delectus. Consequuntur id veritatis odit, aspernatur libero recusandae doloremque necessitatibus aperiam esse eaque consectetur tempore qui quasi, corporis expedita excepturi natus reprehenderit illo temporibus. Veniam eius dolorem cumque! Iusto eum aut et ipsam nemo cum totam delectus explicabo? Quis odio perferendis aliquid facilis ea quaerat vero minima dolorem cumque, est ab! Vero fuga tenetur unde. Perferendis alias, fugit debitis culpa doloremque aperiam molestiae quos incidunt dolore iure officia! Blanditiis sint delectus quam quae nulla.</span></p>
+                    <button class="read-more-btn">Read More</button>
                 <!-- START OF LOCATION SECTION-->
                 <div class="col-md-12 p-2 mb-4" id="location">
                     <h1 class="mx-4 fs-4"><i class="fas fa-map-marked"></i>&nbsp;&nbsp;Located at  &nbsp;<?= $data['business_address']; ?></h1>
@@ -1430,6 +1434,33 @@
         counter = 1;
       }
     }, 5000);
+
+    const readMoreBtn = document.querySelector(".read-more-btn");
+    const text = document.querySelector(".text");
+
+    readMoreBtn.addEventListener("click", (e) => {
+    text.classList.toggle("show-more");
+    if (readMoreBtn.innerText === "Read More") {
+        readMoreBtn.innerText = "Read Less";
+    } else {
+        readMoreBtn.innerText = "Read More";
+    }
+    });
+
+    // *********************
+    // This Code is for only the floating card in right bottom corner
+    // **********************
+
+    const touchButton = document.querySelector(".float-text");
+    const card = document.querySelector(".float-card-info");
+    const close = document.querySelector(".gg-close-r");
+
+    touchButton.addEventListener("click", moveCard);
+    close.addEventListener("click", moveCard);
+
+    function moveCard() {
+    card.classList.toggle("active");
+    }
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.11.1/baguetteBox.min.js"></script>
