8c56d605ff7305419f4ce12fc400fc204dc7f5f7
Melodee21 <106127974+Melodee21@users.noreply.github.com>
2023-01-09 04:07:08 +0800

    try

--- businessview.php
+++ businessview.php
@@ -551,8 +551,8 @@
                                                                 <h6 class="mb-0 "><?= $item['name']; ?></h6>
                                                             </div>  
                                                             <p ><?= $item['cuisinename']; ?> Cuisine<br></p>
-                                                            <p class="text"><br>
-                                                            <span class="moreText "><?= $item['description']; ?></span></p>
+                                                            <div class="descript"><p class="text"><br>
+                                                            <span class="moreText "><?= $item['description']; ?></span></p></div>
                                                             <div class="price">
                                                                 <div class="row">
                                                                     <div class="col">
