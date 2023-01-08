11542e405365f926d3bb1d28f03128ac8565a73b
Melodee21 <106127974+Melodee21@users.noreply.github.com>
2023-01-08 21:53:52 +0800

    1

--- reservation.php
+++ reservation.php
@@ -230,7 +230,7 @@ $(document).on('change','#resource_select', function(){
                         }
                         ?>
 </script> 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 </html>
