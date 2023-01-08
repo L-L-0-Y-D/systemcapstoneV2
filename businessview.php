4f32d1b40339252d20683db9b2ccf3d132849215
Melodee21 <106127974+Melodee21@users.noreply.github.com>
2023-01-09 04:52:53 +0800

    a

--- businessview.php
+++ businessview.php
@@ -550,8 +550,8 @@
                                                             <div class="heading">
                                                                 <h6 class="mb-0 "><?= $item['name']; ?></h6>
                                                             </div>  
-                                                            <div class="descript"><p class="text "><?= $item['cuisinename']; ?> Cuisine<br>
-                                                            <span class="moreText "><?= $item['description']; ?></span></p></div>
+                                                            <div class="descript"><p class="text"><?= $item['cuisinename']; ?> Cuisine<br>
+                                                            <span class="moreText">&nbsp;&nbsp;<?= $item['description']; ?></span></p></div>
                                                             <div class="price">
                                                                 <div class="row text-end">
                                                                     <div class="col-12 text-end mt-2">
