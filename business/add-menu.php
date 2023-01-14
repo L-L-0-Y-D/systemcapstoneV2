0a95eeba95c665efc0d80b5cbdad94acbedbd965
Melodee21 <106127974+Melodee21@users.noreply.github.com>
2023-01-14 13:34:06 +0800

    add menu

--- business/add-menu.php
+++ business/add-menu.php
@@ -22,9 +22,12 @@ if(isset($_GET['id']))
           <a href="menu.php?id=<?= $_SESSION['auth_user']['businessid'];?>" class="back btn-sm btn-close float-end"></a>
           <h6 class="fw-bold">Add Products</h6>
         </div>
+        <div class="col">
+            <a href="javascript:void(0)" class="add-more-form float-end btn btn-primary btn-sm m-3">ADD MORE</a>
+        </div>
         <div class="card-body">
             <form action="code.php" method="POST" enctype="multipart/form-data">
-                <div class="row">
+                <div class="main-form row">
                     <div class="col-md-12">
                         <?php 
                         ?>
@@ -91,8 +94,10 @@ if(isset($_GET['id']))
                         <div class="form-check form-switch">
                             <input class="form-check-input" type="checkbox" id="formCheck-1" name="status" > 
                             <label for="formCheck-1"><strong>Status</strong></label>
+                            <button type="button" class="float-end remove-btn btn btn-danger btn-sm">Remove</button>
                         </div>
                     </div>
+                    <div class="paste-new-forms"></div>
                     <div class="col-md-9">
                         <button type="submit" class="btn save-btn " name="add_product_btn" >Save</button>
                     </div>
@@ -103,8 +108,73 @@ if(isset($_GET['id']))
     </div>
   </div>  
 </div>
+<script>
+        $(document).ready(function () {
 
+            $(document).on('click', '.remove-btn', function () {
+                $(this).closest('.main-form').remove();
+            });
+            
+            $(document).on('click', '.add-more-form', function () {
+                $('.paste-new-forms').append('<div class=" main-form row">\
+                    <div class="col-md-12">\
+                        <?php 
+                        ?>
+                        <input type="hidden" name="businessid" value="<?= $_SESSION['auth_user']['businessid'];?>">\
+                        <?php
+                        ?>
+                    </div>\
+                    <div class="col-md-12">\
+                        <label >Upload Product Image (Image must be greater than 2MB to get a better quality)</label>\
+                        <input type="file" name="image" required class="form-control mb-2">\
+                    </div>\
+                    <div class="col-md-6">\
+                        <label >Product Name:</label>\
+                        <input type="text" name="name" required class="form-control mb-2">\
+                    </div>\
+                    <div class="col-md-6">\
+                        <label>Price:</label>\
+                        <input type="text" name="price" required class="form-control mb-2">\
+                    </div>\
+                    <div class="col-md-6">\
+                        <label >Course Menu:</label>\
+						<select  name="food_type" required class="form-control mb-2">\
+							<option disabled selected hidden> Select your Course Menu </option>\
+							<option value="Appetizer">Appetizer</option>\
+							<option value="Soup">Soup</option>\
+						    <option value="FishDish">Fish Dish</option>\
+							<option value="MeatDish">Meat Dish</option>\
+							<option value="Main">Main Course</option>\
+							<option value="Salad">Salad</option>\
+						    <option value="Dessert">Dessert</option>\
+						    <option value="Drinks">Drinks</option>\
+						</select>\
+                    </div>\
+                    <div class="col-md-6">\
+                        <label >Cuisine Type:</label>\
+						<select class= "border rounded form-select mb-3" name="cuisinename" required>\
+                        <option disabled selected hidden>Select your Type of Cuisine</option>\
+                        <option value="<?= $item ?>"><?= $item ?></option>\
+                        </select>\
+                    </div>\
+                    <div class="col-md-12">\
+                        <label>Product Description:</label>\
+                        <textarea rows="3" name="description" required class="form-control mb-2"></textarea>\
+                    </div>\
+                    <div class="row">\
+                        <div class="col">\
+                            <div class="form-check form-switch">\
+                                <input class="form-check-input" type="checkbox" id="formCheck-1" name="status" > \
+                                <label for="formCheck-1"><strong>Status</strong></label>\
+                                <button type="button" class="float-end remove-btn btn btn-danger btn-sm">Remove</button>\
+                            </div>\
+                        </div>\
+                    </div>\
+                </div>');
+            });
 
+        });
+    </script>
 <?php 
 }
 else
